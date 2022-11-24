da84bf50d491529d8ebbe47329b4e78e7f09e0a9
kahuan vitelli <kahuanvitelli@gmail.com>
2022-11-24 10:22:51 -0300

    exemplos da alura

---
+++ aluraCurso.js
@@ -0,0 +1,33 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+// Texto
+const texto1 = "olá mundo!";
+const texto2 = 'olá mundo';
+const senha = "senhaSuperSegura1234!";
+const StringDeNumero = "412341";
+
+const citacao = "Meu nome é ";
+const meuNome = "leonardo";
+
+
+//concatenação (+)
+
+"console.log(citacao + meuNome);"
+
+//Booleam (True = verdadeiro, false = falso)
+
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto3 = "a";
+const texto4 = "alura";
+
+console.log(texto3 === texto4);
