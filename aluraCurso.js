1f3573a45bc0f962d2287d0432f11080c123834e
kahuan vitelli <kahuanvitelli@gmail.com>
2022-11-30 08:41:17 -0300

    novos códigos

--- aluraCurso.js
+++ aluraCurso.js
@@ -1,4 +1,4 @@
-// tipo Number
+// tipo Number//
 
 const meuNumero = 3;
 
@@ -7,7 +7,7 @@ const segundoNumero = 2;
 
 const operacaoMatematica = primeiroNumero * segundoNumero;
 
-// Texto
+// Texto//
 const texto1 = "olá mundo!";
 const texto2 = 'olá mundo';
 const senha = "senhaSuperSegura1234!";
@@ -17,17 +17,65 @@ const citacao = "Meu nome é ";
 const meuNome = "leonardo";
 
 
-//concatenação (+)
+//concatenação (+)//
 
-"console.log(citacao + meuNome);"
+//"console.log(citacao + meuNome);"
 
 //Booleam (True = verdadeiro, false = falso)
 
-let cadastroAtivado = true;
+//let cadastroAtivado = true;
 
-console.log(primeiroNumero === segundoNumero);
+//const texto3 = "a";
+//const texto4 = "alura";
 
-const texto3 = "a";
-const texto4 = "alura";
+//console.log(texto3 === texto4);
 
-console.log(texto3 === texto4);
+//var//
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+//LET// 
+
+//let forma = 'retangulo'
+//let altura = 5;
+//let comprimento = 7;
+//let area;
+
+//if (forma === 'retangulo') {
+    //area = altura * comprimento;
+//} else {
+    //area = (altura * comprimento) / 2;
+//}
+
+//console.log(area);
+
+// CONST //
+
+//const forma = 'triângulo';
+//const altura = 5;
+//const comprimento = 7;
+//let area;
+
+//if (forma === 'quadrado') {
+ //   area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//console.log(area);
+
+//Tipo de dado // 
+//Booleanos//
+
+//Conversão Implícita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
